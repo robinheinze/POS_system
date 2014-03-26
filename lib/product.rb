dead8d7cfa5c732875e5c98cb1e04eac4441aefd
robinmiller <robin.l.heinze@gmail.com>
2014-03-25 20:17:01 -0700

    move total quantity functionality to model instead of UI

--- lib/product.rb
+++ lib/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   has_many :transaction_items
   has_many :transactions, {:through => :transaction_items}
+
+  def total_quantity
+  	transaction_items.sum('quantity')
+  end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe Product do
   it { should have_many :transactions }
+
+  describe 'total_quantity' do
+  	it 'should return the total quantity sold of this product' do
+  		product = Product.create(:name => 'widget', :price => 5.00)
+  		transaction = Transaction.create(:cashier_id => 2)
+  		transaction_item1 = TransactionItem.create(:product_id => product.id, :transaction_id => transaction.id, :quantity => 2)
+  		transaction_item2 = TransactionItem.create(:product_id => product.id, :transaction_id => transaction.id, :quantity => 3)
+  		product.total_quantity.should eq 5
+  	end
+  end
 end
